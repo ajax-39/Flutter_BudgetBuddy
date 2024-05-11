bb158caba14fdc42c5521436ff6f2265ff4c6a5f
Atharv Jagzap <atharvjagzap@gmail.com>
2024-05-11 10:58:27 +0530

    data model & unique id

---
+++ lib/models/expenses.dart
@@ -0,0 +1,19 @@
+//class as data model for expenses
+
+//get unique id
+import 'package:uuid/uuid.dart';
+
+const uuid = Uuid();
+
+class Expense {
+  Expense({
+    required this.title,
+    required this.date,
+    required this.amount,
+  }) : id = uuid.v4();
+
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+}
