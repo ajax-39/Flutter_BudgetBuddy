b4518d8f16375b98733d63a1ac76736d08d1f253
Atharv Jagzap <atharvjagzap@gmail.com>
2024-05-11 10:47:46 +0530

    home screen to show char and list

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:expense_tracker/expenses.dart';
 
 void main() {
   runApp(
     const MaterialApp(
-      home: Scaffold(),
+      debugShowCheckedModeBanner: false,
+      home: Expenses(),
     ),
   );
 }
